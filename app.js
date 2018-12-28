6c1154e3ff1f1145286b04212de7378cd3449669
JoiHdez <joehdezcon@gmail.com>
2018-12-28 14:38:52 -0600

    Edi1
    clase uno

---
+++ app.js
@@ -0,0 +1,8 @@
+// Codigo de TypeScript
+function saludar(nombre) {
+    console.log("Hola " + nombre);
+}
+var capitan = {
+    nombre: "America"
+};
+saludar(capitan.nombre);
---
+++ app.ts
@@ -0,0 +1,10 @@
+// Codigo de TypeScript
+function saludar( nombre:string ){
+	console.log("Hola " + nombre);
+}
+
+var capitan = {
+	nombre: "America"
+};
+
+saludar(capitan.nombre);
